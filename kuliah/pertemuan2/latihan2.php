6a7885bc2fd6cdfc3b6a057d7edbb7603ec002ad
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-04-11 13:42:45 +0700

    pertemuan2 done

--- kuliah/pertemuan2/latihan2.php
+++ kuliah/pertemuan2/latihan2.php
@@ -1,17 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>latihan php</title>
-</head>
-
-<body>
-    <h1><?php echo "Hello Word!"; ?> <h1>
-
-            <p><?php echo "Rian Hery Juniarto"; ?>
-            <p>
-</body>
-
-</html>
+<?php
+$nama ="Rayhan Faiz Adzkra";
+$email ="rayhanbe123@gmail.com";
+$pekerjaan ="mahasiswa";
+$jurusan ="teknik informatika";
+$universitas ="Universitas Pasundan";
+echo "<h1>$nama<h1>";
+echo "<h4>$email<h4>";
+echo "<h4>$pekerjaan<h4>";
+echo "<h4>$jurusan<h4>";
+echo "<h4>$universitas<h4>";
+echo "<P>saya $nama mahasiswa teknik informatika unpas<P>";
+?>
