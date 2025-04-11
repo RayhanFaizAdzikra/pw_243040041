ed9f64fb6afed6980e9a1c9e74676c0a816fd32a
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-04-11 13:47:37 +0700

    pertemuan4

---
+++ kuliah/pertemuan4/latihan1.php
@@ -0,0 +1,15 @@
+<?php
+$i = 10;
+//while
+//nilaiawal
+//kondisi terminasi /kapan looping berhenti
+//increment/decrement
+while ($i >= 1) {
+    echo "hello word $i <br>";
+    // $i = $i - 1;
+    $i--;
+}
+echo "<hr>";
+for($i = 1; $i <= 10; $i++){
+echo "Hello Word <br>";
+}
