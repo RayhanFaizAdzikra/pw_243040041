2ada738d8b21d179b37a05287e5d39f59b2c1651
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-04-11 13:51:17 +0700

    pertemuan6

---
+++ kuliah/pertemuan6/latihan1.php
@@ -0,0 +1,15 @@
+<?php
+
+// membuat array
+$hari = array ('senin','selasa','rabu');
+$bulan =['januari','februari','maret'];
+$myarray = ['rayhan','18','true'];
+
+//mencetak /menampilakan array
+//menampilan seluruh list (hanya untuk debugging)
+var_dump($hari);
+echo '<br>';
+print_r($bulan);
+echo'<br>';
+//gunakan echo jika ingin mencetak 1 elemen (mengunakan index)
+echo  $hari ;
---
+++ kuliah/pertemuan6/latihan3.php
@@ -0,0 +1,2 @@
+<?php
+$mahasiswa = ['Rayhan','243040041','rayhanbae123@gmail.com','teknik informatika'];
