7630d95e730547f1de3a0bed8a44c5048b0af7d1
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-04-29 16:01:03 +0700

    selesai

--- tugas/tugas6/6c.php
+++ tugas/tugas6/6c.php
@@ -1,4 +1,4 @@
-<?php
+<?ph
 session_start();
 $error = "";
 
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
 
-    if ($username === "Hery" && $password === "123") {
+    if ($username === "rayhan" && $password === "123") {
         $_SESSION['loggedin'] = true;
 
         exit();
