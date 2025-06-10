5705968e7033254d9d7ac8cd48ca988e762b0b6b
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-06-10 15:45:12 +0700

    pertemuan13

---
+++ kuliah/pertemuan13/funtion.php
@@ -0,0 +1,74 @@
+
+<?php
+function koneksi()
+{
+    $conn = mysqli_connect('localhost', 'root', '', 'pw2024_243040040');
+    return $conn;
+}
+
+function query($query)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, $query);
+
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+
+
+function tambah($data)
+{
+
+    $conn = koneksi();
+
+    $nim = htmlspecialchars($data["nim"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data['email']);
+    $jurusan = htmlspecialchars($data['jurusan']);
+    $gambar = htmlspecialchars($data['gambar']);
+
+    $query = "INSERT INTO mahasiswa (nim, nama, email, jurusan, gambar)
+              VALUES ('$nim', '$nama', '$email', '$jurusan', '$gambar')";
+
+    mysqli_query($conn, $query);
+
+
+    return mysqli_affected_rows($conn);
+}
+
+
+function hapus($id)
+{
+    $conn = koneksi();
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+    $conn = koneksi();
+    $id = $data["id"];
+    $nim = htmlspecialchars($data["nim"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data['email']);
+    $jurusan = htmlspecialchars($data['jurusan']);
+    $gambar = htmlspecialchars($data['gambar']);
+
+    $query = "UPDATE mahasiswa SET
+                nim = '$nim',
+                nama = '$nama',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+                WHERE id = $id
+            ";
+
+    mysqli_query($conn, $query);
+
+
+    return mysqli_affected_rows($conn);
+}
